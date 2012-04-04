e3e0aecef16b963580663fba19416cf6a5623c13
fukamachi <e.arrows@gmail.com>
2012-04-04 21:48:55 +0900

    added 2 features which was added to Caveman today. - rewrote `next-route' as a something like "continuation". this allows to use the return value in the previous route. - returns 200 for HEAD.

--- src/app.lisp
+++ src/app.lisp
@@ -6,6 +6,7 @@
 (in-package :cl-user)
 (defpackage ningle.app
   (:use :cl
+        :anaphora
         :clack
         :clack.request
         :ningle.middleware.context)
@@ -19,6 +20,9 @@
 (in-package :ningle.app)
 
 (cl-syntax:use-syntax :annot)
+
+(defparameter *next-route-function* nil
+  "A function called when `next-route' is invoked. This will be overwritten in `dispatch-with-rules'.")
 
 @export
 (defclass <app> (<component>)
@@ -36,30 +40,28 @@
 (defmethod call ((this <app>) env)
   "Overriding method. This method will be called for each request."
   @ignore env
+  (dispatch-with-rules (reverse (routing-rules this))))
+
+(defun dispatch-with-rules (rules)
   (let* ((req *request*)
          (path-info (path-info req))
          (method (request-method req)))
-    (loop for (rule controller) in (reverse (routing-rules this))
-          do (multiple-value-bind (matchp params)
-                 (match rule method path-info)
-               (when matchp
-                 (setf (slot-value req 'clack.request::query-parameters)
-                       (append
-                        params
-                        (slot-value req 'clack.request::query-parameters)))
-
-                 (if (functionp controller)
-                     (let ((res (call controller (parameter req))))
-                       (unless (eq res (next-route))
-                         (return res)))
-                     (return controller))))
-          finally
-          (progn (setf (clack.response:status *response*) 404)
-                 nil))))
-
-(defmethod match-url-rule-p ((rule <url-rule>) url-rule method)
-  (and (eq (clack.util.route::request-method rule) method)
-       (string= (clack.util.route::url rule) url-rule)))
+    (acond
+      ((and rules
+            (member-rule path-info method rules))
+       (destructuring-bind ((url-rule controller) &rest other-rules) it
+         (let ((*next-route-function* #'(lambda () (dispatch-with-rules other-rules))))
+           (multiple-value-bind (_ params)
+               (match url-rule method path-info)
+             @ignore _
+             (setf (slot-value req 'clack.request::query-parameters)
+                   (append
+                    params
+                    (slot-value req 'clack.request::query-parameters)))
+             (if (functionp controller)
+                 (funcall controller (parameter req))
+                 controller)))))
+      (t (not-found)))))
 
 @export
 (defmethod route ((this <app>) url-rule &key (method :get))
@@ -80,11 +82,27 @@
 
   controller)
 
-(defparameter +next-route+ '#:next-route)
-
 @export
 (defun next-route ()
-  +next-route+)
+  (funcall *next-route-function*))
+
+@export
+(defun not-found ()
+  "An action when no routing rules are found."
+  (setf (clack.response:status *response*) 404)
+  nil)
+
+(defmethod match-url-rule-p ((rule <url-rule>) url-rule method)
+  (and (eq (clack.util.route::request-method rule) method)
+       (string= (clack.util.route::url rule) url-rule)))
+
+(defun member-rule (path-info method rules)
+  (member-if #'(lambda (rule)
+                 (or (match rule method path-info)
+                     (and (eq method :HEAD)
+                          (match rule :GET path-info))))
+             rules
+             :key #'car))
 
 (doc:start)
 
@@ -95,6 +113,17 @@ Ningle.App - Ningle Application Class.
 @doc:SYNOPSIS "
     (defclass <myapp-app> (<app>) ())
     (defvar *app* (make-instance '<myapp-app>))
+    
+    (setf (route *app* \"/\")
+          \"Welcome to ningle!\")
+    
+    (setf (ningle:route *app* \"/login\" :method :POST)
+          #'(lambda (params)
+              (if (authorize (getf params :|username|)
+                             (getf params :|password|))
+                \"Authorized!\"
+                \"Failed...Try again.\")))
+    
     (call *app*)
 "
 
